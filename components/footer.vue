867b5c5e0b6c02228a520c0feb9cce6965134163
bhaskarkishor <bhaskarkishor2301@gmail.com>
2020-11-22 00:31:41 +0530

    merge

--- components/footer.vue
+++ components/footer.vue
@@ -1,11 +1,4 @@
 <template>
-<<<<<<< HEAD
-<v-footer class="accent">
-    <v-container class="text-center small-text pb-2">
-      Made By Bhaskar Kishor | <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-container>
-</v-footer>
-=======
 <footer class="accent" app>
     <v-card flat class="accent text-center mx-auto">
       <v-card-text>
@@ -13,7 +6,6 @@
       </v-card-text>
     </v-card>
 </footer>
->>>>>>> branchname
 </template>
 
 <script>
